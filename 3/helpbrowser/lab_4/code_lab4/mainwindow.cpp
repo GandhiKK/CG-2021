eab8a017991b4e37ff52d7004dcb3d375a233f9d
Konstantin <gandhi.noir@mail.ru>
2021-03-14 16:06:58 +0300

    1-2-3

---
+++ 3/helpbrowser/lab_4/code_lab4/mainwindow.cpp
@@ -0,0 +1,38 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setWindowTitle("Примитивы OpenGL");
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_horizontalSlider_sliderMoved(int position)
+{
+    ui->widget->setd(position);
+    ui->widget->updateGL();
+}
+
+void MainWindow::on_checkBox_clicked(bool checked)
+{
+    ui->widget->setb(checked);
+    ui->widget->updateGL();
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->widget->clear();
+    ui->widget->updateGL();
+}
+
+
+
